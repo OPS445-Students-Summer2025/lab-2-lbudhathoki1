d1c53fbeb422591ca6924e665c8dd69fdca90687
Lalit Budhathoki <lbudhathoki@myseneca.ca>
2025-06-16 15:35:43 -0400

    All completed lab

---
+++ lab2_if_test.py
@@ -0,0 +1,9 @@
+if True:
+    print('This print is apart of the if statement')
+if False:
+    print('This first print statement will never run')
+    print('This second print statement will also not run')
+print('This print statement will run')
+password = input('Please enter a secret password: ')
+if password == 'P@ssw0rd':
+    print('You have successfully used the right password')
