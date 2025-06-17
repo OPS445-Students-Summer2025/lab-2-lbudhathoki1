ed3a246d263fa4481b910018859e96a03d7f572c
Lalit Budhathoki <lbudhathoki@myseneca.ca>
2025-06-16 23:48:57 -0400

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Lalit Budhathoki
+# Author ID: lbudhathoki
+# Date Created: 2025/06/16
+import sys
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
